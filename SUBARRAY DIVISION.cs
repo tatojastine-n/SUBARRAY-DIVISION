ad37b4ce3f20b36190b17e7aaddbc05802f16fc5
tatojastine-n <jajanicoletato@gmail.com>
2025-10-02 15:01:05 +0800

    SUBARRAY DIVISION.cs

--- SUBARRAY DIVISION.cs
+++ SUBARRAY DIVISION.cs
@@ -17,7 +17,18 @@ vector<string> split(const string &);
  */
 
 int birthday(vector<int> s, int d, int m) {
-
+    int count = 0;
+    int n = s.size();
+    for (int i = 0; i <= n - m; i++) {
+        int sum = 0;
+        for (int j = i; j < i + m; j++) {
+            sum += s[j];
+        }
+        if (sum == d) {
+            count++;
+        }
+    }
+    return count;
 }
 
 int main()
